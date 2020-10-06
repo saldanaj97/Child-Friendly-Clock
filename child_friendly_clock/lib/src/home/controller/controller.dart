8243be011b3e4de03fc97360e8ed7ad1b4797ff0
Juan Saldana <saldanaj97@gmail.com>
2020-10-06 12:14:52 -0500

    Setup file structure for MVC architecture

---
+++ child_friendly_clock/lib/src/home/controller/controller.dart
@@ -0,0 +1,23 @@
+import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:child_friendly_clock/src/home/model/model.dart';
+
+class Controller extends ControllerMVC {
+  factory Controller() {
+    if (_this == null) _this = Controller._();
+    return _this;
+  }
+  static Controller _this;
+
+  Controller._();
+
+  int get counter => Model.counter;
+  void incrementCounter() {
+    /// The Controller knows how to 'talk to' the Model. It knows the name, but Model does the work.
+    Model.incrementCounter();
+  }
+
+  void decrementCounter() {
+    /// The Controller knows how to 'talk to' the Model. It knows the name, but Model does the work.
+    Model.decrementCounter();
+  }
+}
