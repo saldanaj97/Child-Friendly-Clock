80fa25f4e91ad2fad00eddaf2b5c661175275ea4
Juan Saldana <saldanaj97@gmail.com>
2020-10-27 17:59:25 -0500

    Deleted unused files

--- child_friendly_clock/lib/src/alarm/view/CreateAlarm.dart
+++ child_friendly_clock/lib/src/alarm/view/CreateAlarm.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:child_friendly_clock/src/alarm/model/Alarm.dart';
 import 'package:child_friendly_clock/src/alarm/utils/database.dart';
+import 'package:flutter_picker/flutter_picker.dart';
 
 class SaveButton extends StatefulWidget {
   final VoidCallback save;
@@ -80,28 +81,79 @@ class _CreateAlarmState extends State<CreateAlarm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(255, 45, 45, 70),
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          title: Text('Add Alarm'),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 55, 55, 70),
+      backgroundColor: Color.fromARGB(255, 45, 45, 70),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-          child: Column(children: <Widget>[
+        title: Text('Add Alarm'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 55, 55, 70),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 15, 30, 0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(),
+                NumberPicker.integer(
+                    initialValue: newAlarm.hour,
+                    minValue: 1,
+                    maxValue: 12,
+                    listViewWidth: 100.0,
+                    haptics: true,
+                    onChanged: (newValue) => setState(() => newAlarm.hour = newValue)),
+                Text(
+                  ':',
+                  style: TextStyle(
+                    color: Colors.lightBlue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                NumberPicker.integer(
+                    initialValue: newAlarm.minute,
+                    minValue: 0,
+                    maxValue: 59,
+                    zeroPad: true,
+                    listViewWidth: 60.0,
+                    onChanged: (newValue) => setState(() => newAlarm.minute = newValue)),
+                ToggleButtons(
+                  children: [Text('AM'), Text('PM')],
+                  isSelected: _selections,
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int btnIndex = 0; btnIndex < _selections.length; btnIndex++) {
+                        if (btnIndex == index) {
+                          _selections[btnIndex] = true;
+                          if (btnIndex == 0)
+                            newAlarm.period = "AM";
+                          else
+                            newAlarm.period = "PM";
+                        } else {
+                          _selections[btnIndex] = false;
+                        }
+                      }
+                    });
+                  },
+                )
+              ],
+            ),
+            Divider(color: Colors.grey[400], height: 5.0),
             Row(
               children: <Widget>[
-                Text('Name:',
-                    style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0,
-                    )),
+                Text(
+                  'Name:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 28.0,
+                  ),
+                ),
                 Expanded(
                   child: TextField(
                     decoration: InputDecoration(border: InputBorder.none, hintText: 'enter name', hintStyle: TextStyle(color: Colors.grey[500])),
@@ -127,88 +179,64 @@ class _CreateAlarmState extends State<CreateAlarm> {
                 )
               ],
             ),
-            Divider(
-              color: Colors.grey[400],
-              height: 5.0,
-            ),
-            Row(
-              children: <Widget>[
-                Text('Time:',
-                    style: TextStyle(
-                      color: Colors.white,
-                      letterSpacing: 2.0,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28.0,
-                    )),
-                SizedBox(width: 20.0),
-                NumberPicker.integer(
-                    initialValue: newAlarm.hour,
-                    minValue: 1,
-                    maxValue: 12,
-                    listViewWidth: 35.0,
-                    onChanged: (newValue) => setState(() => newAlarm.hour = newValue)),
-                Text(':',
-                    style: TextStyle(
-                      color: Colors.lightBlue,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    )),
-                NumberPicker.integer(
-                    initialValue: newAlarm.minute,
-                    minValue: 0,
-                    maxValue: 59,
-                    zeroPad: true,
-                    listViewWidth: 60.0,
-                    onChanged: (newValue) => setState(() => newAlarm.minute = newValue)),
-                ToggleButtons(
-                    children: [Text('AM'), Text('PM')],
-                    isSelected: _selections,
-                    onPressed: (int index) {
-                      setState(() {
-                        for (int btnIndex = 0; btnIndex < _selections.length; btnIndex++) {
-                          if (btnIndex == index) {
-                            _selections[btnIndex] = true;
-                            if (btnIndex == 0)
-                              newAlarm.period = "AM";
-                            else
-                              newAlarm.period = "PM";
-                          } else {
-                            _selections[btnIndex] = false;
-                          }
-                        }
-                      });
-                    })
-              ],
-            ),
-            Divider(color: Colors.grey[400], height: 5.0),
             SizedBox(height: 20.0),
             Row(
               children: [
                 Expanded(
-                    child: FlatButton(
-                  textColor: Colors.lightBlue,
-                  shape: CircleBorder(),
-                  child: Text('Cancel',
+                  child: FlatButton(
+                    textColor: Colors.lightBlue,
+                    shape: CircleBorder(),
+                    child: Text(
+                      'Cancel',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 20.0,
-                      )),
-                  color: Colors.white,
-                  onPressed: () => Navigator.of(context).pop(),
-                  height: 100,
-                )),
+                      ),
+                    ),
+                    color: Colors.white,
+                    onPressed: () => Navigator.of(context).pop(),
+                    height: 100,
+                  ),
+                ),
                 //savebutton
                 SaveButton(
-                    active: canSave,
-                    save: () {
-                      newAlarm.name = _nameController.text;
-                      DBProvider.db.newAlarm(newAlarm);
-                      widget.clickCallback();
-                      Navigator.pop(context);
-                    })
+                  active: canSave,
+                  save: () {
+                    newAlarm.name = _nameController.text;
+                    DBProvider.db.newAlarm(newAlarm);
+                    widget.clickCallback();
+                    Navigator.pop(context);
+                  },
+                )
               ],
             )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget timeSelector() {
+    showPickerNumber(BuildContext context) {
+      return Picker(
+          adapter: NumberPickerAdapter(data: [
+            NumberPickerColumn(begin: 0, end: 999),
+            NumberPickerColumn(begin: 100, end: 200),
           ]),
-        ));
+          delimiter: [
+            PickerDelimiter(
+                child: Container(
+              width: 30.0,
+              alignment: Alignment.center,
+              child: Icon(Icons.more_vert),
+            ))
+          ],
+          hideHeader: true,
+          title: new Text("Please Select"),
+          onConfirm: (Picker picker, List value) {
+            print(value.toString());
+            print(picker.getSelectedValues());
+          }).showDialog(context);
+    }
   }
 }
